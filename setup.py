a35f7f06bc9fbb828b9e35f2050a9309bc4dc5ae
Andrey Kislyuk <kislyuk@gmail.com>
2014-01-05 23:04:24 -0800

    v0.1.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = []
 
 setup(
     name='ensure',
-    version='0.1.4',
+    version='0.1.5',
     url='https://github.com/kislyuk/ensure',
     license='Apache Software License',
     author='Andrey Kislyuk',
