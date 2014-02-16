a0d77ee1d7d267f76ce8507200578b7aade807f7
Andrey Kislyuk <kislyuk@gmail.com>
2014-02-15 17:35:32 -0800

    v0.1.6

---
+++ Changes.rst
@@ -0,0 +1,4 @@
+Version 0.1.6 (2014-02-15)
+--------------------------
+- Begin tracking changes in changelog.
+- Add support for function annotation enforcement.
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = []
 
 setup(
     name='ensure',
-    version='0.1.5',
+    version='0.1.6',
     url='https://github.com/kislyuk/ensure',
     license='Apache Software License',
     author='Andrey Kislyuk',
