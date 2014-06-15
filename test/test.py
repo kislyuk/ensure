3921e1ef3e359b5595591773de41d20266aee596
Harrison Metzger <harrisonmetz@gmail.com>
2014-06-14 22:42:56 -0500

    pep8 fixes

--- test/test.py
+++ test/test.py
@@ -3,10 +3,16 @@
 
 from __future__ import print_function, unicode_literals
 
-import os, sys, unittest, collections, copy, re
+import os
+import sys
+import unittest
+import collections
+import copy
+import re
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from ensure import *
+
 
 class TestEnsure(unittest.TestCase):
     def test_basic_ensure_statements(self):
